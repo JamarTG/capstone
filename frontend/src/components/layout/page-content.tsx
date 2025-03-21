4586b975abb72e2435c321872cb78ff0c0c5a8f9
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-21 10:57:21 -0500

    Add Back Button to Page COntent

--- frontend/src/components/layout/page-content.tsx
+++ frontend/src/components/layout/page-content.tsx
@@ -1,11 +1,40 @@
+import { useNavigate } from "react-router-dom";
+
 interface PageContentProps {
   children: React.ReactNode;
   title: string;
 }
 const PageContent: React.FC<PageContentProps> = ({ children, title }) => {
+
+  const navigate = useNavigate();
   return (
     <div>
-      <h1 className="mb-2 text-2xl font-bold">{title.toLocaleLowerCase()}</h1>
+      <h1 className="mb-2 text-2xl font-bold flex items-center">
+        <svg
+          onClick={() => {
+            navigate(-1);
+          }}
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-arrow-left cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110 active:scale-90"
+        >
+          <line
+            x1="19"
+            y1="12"
+            x2="5"
+            y2="12"
+          ></line>
+          <polyline points="12 19 5 12 12 5"></polyline>
+        </svg>{" "}
+        {title.toLocaleLowerCase()}
+      </h1>
       <section
         style={{ width: "90vw", height: "85vh" }}
         className=" border-slate-200 rounded-lg  flex flex-col h-full p-4 gap-5 "
