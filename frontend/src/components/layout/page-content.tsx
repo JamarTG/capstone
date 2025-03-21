b8cb4ff1b2c5673d46390393c3b3935a9e5b6984
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-21 10:17:26 -0500

    Edit Page Content Layout Container

--- frontend/src/components/layout/page-content.tsx
+++ frontend/src/components/layout/page-content.tsx
@@ -8,7 +8,7 @@ const PageContent: React.FC<PageContentProps> = ({ children, title }) => {
       <h1 className="mb-2 text-2xl font-bold">{title.toLocaleLowerCase()}</h1>
       <section
         style={{ width: "90vw", height: "85vh" }}
-        className="border-2 border-slate-300 rounded-lg bg-gray-50 flex flex-col h-full p-4 gap-5 "
+        className=" border-slate-200 rounded-lg bg-gray-100 flex flex-col h-full p-4 gap-5 "
       >
         {children}
       </section>
