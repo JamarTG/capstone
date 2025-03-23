9d0045cf4e9809bd91b50331f4d16d6074d279ba
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-23 11:10:48 -0500

    fix: resolve strokeWidth & strokeLineCap warnings

--- frontend/src/components/layout/page-content.tsx
+++ frontend/src/components/layout/page-content.tsx
@@ -20,9 +20,9 @@ const PageContent: React.FC<PageContentProps> = ({ children, title }) => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="feather feather-arrow-left cursor-pointer transition-transform duration-200 ease-in-out transform hover:scale-110 active:scale-90"
         >
           <line
