88be1e69fa214ee6826bbcedd2eca4c14cda4a6f
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-23 17:12:13 -0500

    refactor: create function for text and bg text weakness coloring

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,9 +1,15 @@
-const getWeaknessColor = (percentage: number): string => {
+const getTextWeaknessColor = (percentage: number): string => {
   if (percentage >= 80) return "text-red-500";
   if (percentage >= 60) return "text-yellow-500";
   return "text-green-500";
 };
 
+const getBgWeaknessColor = (percentage: number): string => {
+  if (percentage >= 80) return "bg-red-500";
+  if (percentage >= 60) return "bg-yellow-500";
+  return "bg-green-500";
+};
 
 
-  export { getWeaknessColor };
+
+  export { getBgWeaknessColor, getTextWeaknessColor };
