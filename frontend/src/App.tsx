a86293eaa76e517a8ad33faa56db42f3a0ead177
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-23 22:57:20 -0500

    chore:remove unused 'App.css'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
