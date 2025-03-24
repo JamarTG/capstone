5a2019156e143dca97f14aeea9c7927a3fe0c6ad
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-24 17:30:14 -0500

    chore: prevent selecting user salt

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -4,26 +4,24 @@ import { IUser } from "../types/model";
 
 const userSchema = new mongoose.Schema<IUser>({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, select:false},
-  firstName : {type: String, required: true},
-  lastName: {type: String, required: true},
-  salt: { type: String, select:false},
+  password: { type: String, required: true, select: false },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  salt: { type: String, select: false },
   createdAt: { type: Date, default: Date.now },
 });
 
 userSchema.pre("save", function (next) {
-  if (!this.isModified("password")) return next();    
+  if (!this.isModified("password")) return next();
   this.salt = crypto.randomBytes(16).toString("hex");
-  this.password = crypto
-    .pbkdf2Sync(this.password, this.salt, 1000, 64, "sha512")
-    .toString("hex");
+  this.password = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, "sha512").toString("hex");
 
   return next();
 });
 
 userSchema.methods.comparePassword = function (password: IUser["password"]): boolean {
   const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, "sha512").toString("hex");
-  return hash == this.password
+  return hash == this.password;
 };
 
 export default mongoose.model<IUser>("User", userSchema);
