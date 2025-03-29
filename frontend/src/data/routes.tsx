73f246382b82087c78e0ee9681db4dbd0eb5cc01
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-29 16:55:31 -0500

    a lot: add images and some dependencies

--- frontend/src/data/routes.tsx
+++ frontend/src/data/routes.tsx
@@ -5,8 +5,8 @@ import Quiz from "../pages/quiz";
 import QuizHistory from "../pages/archive";
 import Settings from "../pages/settings";
 import StudyPlan from "../pages/study-plan";
-import NotFound from "../components/misc/not-found";
 import { JSX, ReactNode } from "react";
+import NotFound from "../pages/not-found";
 
 interface RouteObject {
   path: string;
@@ -19,14 +19,10 @@ const routes: Routes = {
   home: { path: "/", element: <StudyPlan />, layout: SidebarLayout },
   login: { path: "/login", element: <Login /> },
   register: { path: "/register", element: <Register /> },
-  quizHistory: { path: "/quiz-history", element: <QuizHistory />, layout: SidebarLayout },
+  quizHistory: { path: "/archive", element: <QuizHistory />, layout: SidebarLayout },
   quiz: { path: "/quiz", element: <Quiz />, layout: SidebarLayout },
   notFound: { path: "*", element: <NotFound /> },
   settings: { path: "/settings", element: <Settings />, layout: SidebarLayout },
 };
 
 export default routes;
-
-
-// Type '{ children: Element; }' has no properties in common with type 'IntrinsicAttributes'.ts(2559)
-// (parameter) Layout: React.FC<{}>
