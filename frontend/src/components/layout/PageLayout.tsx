6c56c5ece3aea73214e2772937434ec4369b6519
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-03-30 01:32:22 -0500

    style: remove inline styles in page layout

--- frontend/src/components/layout/PageLayout.tsx
+++ frontend/src/components/layout/PageLayout.tsx
@@ -1,23 +1,29 @@
 import { ReactNode } from "react";
+import ConditionalSVG from "../ConditionalSVG";
 
 interface PageContentProps {
   children: ReactNode;
   title: string;
-  svg?: ReactNode; 
+  path: string;
 }
 
-const PageLayout: React.FC<PageContentProps> = ({ children, title, svg }) => {
+const PageLayout: React.FC<PageContentProps> = ({ children, title, path }) => {
   return (
     <div>
-      <h1 className="p-2 cursor-pointer flex justify-start items-center text-3xl">
+      <div className="p-2 cursor-pointer flex justify-start items-center text-3xl">
         <span className="mr-3">
-          {svg}
+          {path && (
+            <ConditionalSVG
+              path={path}
+              size={50}
+            />
+          )}
         </span>
-        <h2 className="text-gray-500 text-2xl">{title}</h2>
-      </h1>
-      <section
-        style={{ width: "90vw", height: "90vh" }}
-        className="flex flex-col h-screen p-4 gap-5  border-t-2 border-slate-200"
+        <h1 className="text-gray-500 text-2xl">{title}</h1>
+      </div>
+
+      <section       
+        className="page-content-dim flex flex-col h-screen p-4 gap-5  border-t-2 border-slate-200"
       >
         {children}
       </section>
