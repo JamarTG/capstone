469ecb6abbf01e24611cd8eea2a0cff4ab5070dc
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-04-01 08:38:33 -0500

    refactor: cahnge image import style

--- frontend/src/components/layout/AuthLayout.tsx
+++ frontend/src/components/layout/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import logo from "/croppedAppLogoIcon.png";
 
 interface AuthProps {
   children?: ReactNode;
@@ -11,14 +12,12 @@ const AuthLayout: React.FC<AuthProps> = ({ children, title }) => {
       <div className="w-full lg:w-1/2 h-screen flex flex-col justify-center items-center">
         <header className="flex flex-col gap-5 justify-center items-center">
           <img
-            src="/croppedAppLogoIcon.png"
+            src={logo}
             className="w-24"
             alt=""
           />
-
           <h1 className="mt-[-10px] flex items-end justify-end text-center text-3xl/9 font-bold tracking-tight text-slate-300">{title}</h1>
         </header>
-
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">{children}</div>
       </div>
       <div className="hidden lg:block lg:w-1/2">
