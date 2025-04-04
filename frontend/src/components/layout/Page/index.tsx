5f83b72f54f9737c9da34d8450c3855086e6051a
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-04-04 18:26:14 -0500

    style: add white background to page content header

--- frontend/src/components/layout/Page/index.tsx
+++ frontend/src/components/layout/Page/index.tsx
@@ -10,7 +10,7 @@ const PageLayout: React.FC<PageLayoutProps> = ({ children, title }) => {
     <div className="min-h-screen bg-white">
 
       <header className="sticky top-0 z-10 border-b border-gray-200">
-        <div className="container mx-auto px-4 py-4">
+        <div className="bg-white container mx-auto px-4 py-4">
           <h1 className="text-lg font-semibold text-slate-600">
             {title}
           </h1>
