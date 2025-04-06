3774f714cf08bf9307864074232a0aa866e248fc
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-04-06 17:45:51 -0500

    style: remove div with border

--- frontend/src/components/layout/Page/index.tsx
+++ frontend/src/components/layout/Page/index.tsx
@@ -9,9 +9,9 @@ const PageLayout: React.FC<PageLayoutProps> = ({ children, title }) => {
   return (
     <div className="min-h-screen bg-white">
 
-      <header className="sticky top-0 z-10 border-b border-gray-200">
+      <header className="sticky top-0 z-10 border-gray-200">
         <div className="bg-white container mx-auto px-4 py-4">
-          <h1 className="text-lg font-semibold text-slate-600">
+          <h1 className="text-4xl font-semibold text-slate-600">
             {title}
           </h1>
         </div>
