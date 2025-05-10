9790e12a1e8d5dab75ac73db45c98dc8b82ccd25
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-05-09 19:58:46 -0500

    fix: load icon from constant

--- frontend/src/pages/Archive/index.tsx
+++ frontend/src/pages/Archive/index.tsx
@@ -2,8 +2,7 @@ import PageLayout from "../../components/layout/Page";
 import QuizHistoryList from "./QuizHistoryList";
 import useAuthRedirect from "../../hooks/useAuthRedirect";
 import SectionHeader from "../../components/SectionHeader";
-import { mdiHistory } from "@mdi/js";
-
+import { MDI_ICONS } from "../../icons";
 const QuizHistory = () => {
   useAuthRedirect();
 
@@ -11,7 +10,7 @@ const QuizHistory = () => {
     <PageLayout title="History">
       <div className="flex flex-col justify-center gap-3">
         <SectionHeader
-          iconPath={mdiHistory}
+          iconPath={MDI_ICONS.history}
           title={"History List"}
         />
         <QuizHistoryList />
